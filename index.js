8e424de7072d9a8bc2d000f31c58c66a1b4ad71f
Samrat Ahmmed <samratahammed29@gmail.com>
2023-11-25 15:11:11 +0600

    add user collection

--- index.js
+++ index.js
@@ -36,13 +36,21 @@ async function run() {
       res.send(result);
     });
 
-// users related .........................
+    // users related .........................
 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
 
+      const existingUser = await usersCollection.findOne(query);
 
-app.post("/users",async(req,res)=.{
-    
-})
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(user);
+
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
